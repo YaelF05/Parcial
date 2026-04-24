7fb1b8d581b4b59be43ffcb79fbdfe3b80d1aad7
YaelF05 <yaelfrnc@gmail.com>
2026-04-24 10:50:56 -0600

    feat: controller con saludo y calificacion

---
+++ src/main/java/com/example/demo/SaludoController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class SaludoController {
+
+    @GetMapping("/saludo")
+    public String saludo() {
+        return "Hola profe pongame 10, soy Yael :)";
+    }
+}
